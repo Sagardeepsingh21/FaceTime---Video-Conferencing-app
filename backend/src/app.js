e712869ad0233c665b2e3687ab16d9fd00016312
Sagardeep Singh <sagardeepsingh@Sagardeeps-MacBook.local>
2026-07-16 18:22:18 +0530

    Added health check endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,9 +14,14 @@ import userRoutes from "./routes/users.routes.js";
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "FaceTime Backend is running 🚀"
+    });
+});
 
-
-app.set("port", (process.env.PORT || 8000))
+app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -29,7 +34,7 @@ const connectionDb = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+        console.log(`Listening on port ${app.get("port")}`);
     });
 
 
