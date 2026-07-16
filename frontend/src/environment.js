f635c25889ae4d09f40482c1abcf090d9b03bab1
Sagardeep Singh <sagardeepsingh@Sagardeeps-MacBook.local>
2026-07-16 18:36:00 +0530

    Updated frontend to use deployed backend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://facetime-video-conferencing-app.onrender.com"
 
-    "http://localhost:8000"
+    :"http://localhost:8000";
 
 
 export default server;
